358c99288b1eebe3f75a73803ed79f57d4cf1506
rumiyevskiy <rumiyevskiy@gmail.com>
2025-01-08 14:36:53 +0200

    finish page home

---
+++ src/scripts/main.js
@@ -0,0 +1,27 @@
+
+// header
+
+const headerEl = document.querySelector('.header');
+
+window.addEventListener('scroll', () => {
+
+  if (window.scrollY > headerEl.offsetHeight) {
+    headerEl.classList.add('scrolled');
+  } else {
+    headerEl.classList.remove('scrolled');
+  }
+});
+
+// slider
+
+const swiper = new Swiper('.feedback .swiper', {
+    
+  slidesPerView: 2,
+    
+    navigation: {
+      nextEl: '.slider-btn--next',
+      prevEl: '.slider-btn--prev',
+    },
+  
+    
+  });
