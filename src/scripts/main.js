684bfa106b42f6d4e60e9ad5aa9d876366ea69af
rumiyevskiy <rumiyevskiy@gmail.com>
2025-01-13 17:22:23 +0200

    add page services

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -24,4 +24,33 @@ const swiper = new Swiper('.feedback .swiper', {
     },
   
     
+});
+  
+// tabs
+
+const showTabs = (tabsNav, tabsContent) => {
+  tabsNav?.forEach((navLink, index) => {
+    navLink?.addEventListener('click', () => {
+      tabsNav?.forEach(link => {
+        link.classList.remove('active');
+      });
+    
+
+    navLink.classList.add('active');
+
+    let navIndex = index;
+
+      tabsContent.forEach((contentItem, index) => {
+        contentItem.classList.remove('active');
+
+        if (index === navIndex) {
+          contentItem.classList.add('active');
+        }
+      });
+    });
   });
+};
+
+const tabsNav = document.querySelectorAll('.bikes .tabs-nav__link');
+const tabsContent = document.querySelectorAll('.bikes .tabs-content');
+showTabs(tabsNav, tabsContent);
