da774e94f817321e4867abb93eff4d03fefbf863
rumiyevskiy <rumiyevskiy@gmail.com>
2025-01-20 13:43:38 +0200

    adapted the home page

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -54,3 +54,30 @@ const showTabs = (tabsNav, tabsContent) => {
 const tabsNav = document.querySelectorAll('.bikes .tabs-nav__link');
 const tabsContent = document.querySelectorAll('.bikes .tabs-content');
 showTabs(tabsNav, tabsContent);
+
+// burger
+
+const burgerBtnEl = document.querySelector('.burger');
+
+const handleBurgerClick = () => {
+  burgerBtnEl.classList.toggle('active');
+}
+
+document.addEventListener('click', (e) => {
+
+  //   Вибір залежить від конкретної задачі:
+
+  // Якщо вам потрібно, щоб подія оброблялась незалежно від того, чи клік був на вкладеному елементі всередині .burger, використовуйте closest('.burger').
+  // Якщо важливо реагувати тільки на сам елемент із класом burger (і ви впевнені, що кліки на вкладені елементи не потрібні), використовуйте classList.contains('burger').
+  // Для навігаційних меню або схожих елементів зазвичай кращим є перший варіант із closest, тому що він забезпечує більш універсальну роботу.
+
+  if (e.target.closest('.burger')) {
+    handleBurgerClick();
+  }
+
+  // тут крапки в ім'ї класу НЕ повинно бути
+  // if (e.target.classList.contains('burger')) {
+  //   handleBurgerClick();
+  // }
+
+});
